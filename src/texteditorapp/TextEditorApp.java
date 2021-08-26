2cd8ae4ba063459009fc354ca23a0282e72a0399
conta <conta@192.168.178.164>
2021-08-26 12:15:57 +0100

    Changes

--- src/texteditorapp/TextEditorApp.java
+++ src/texteditorapp/TextEditorApp.java
@@ -1,20 +1,40 @@
 package texteditorapp;
 
+import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 public class TextEditorApp extends JFrame implements ActionListener {
-
+    
+    JTextArea textArea;
+    JScrollPane scrollPane;
+    
     TextEditorApp() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("Bro text editor");
-        this.setSize(500,500);
+        this.setSize(500, 500);
         this.setLayout(new FlowLayout());
-
+        this.setLocationRelativeTo(null);
+        
+        textArea = new JTextArea();
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+        textArea.setFont(new Font("Arial", Font.PLAIN, 20));
+        
+        scrollPane = new JScrollPane(textArea);
+        
+        
+        
+        this.add(textArea);
+        this.setVisible(true);
+        
     }
-
+    
     @Override
     public void actionPerformed(ActionEvent e) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
